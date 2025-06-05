40f1bf5d7e8996c907c254ea32a7e0a2e5f47ff5
Francisco Rotundo <francorotundo90@gmail.com>
2025-06-05 14:35:12 -0400

    feat: Get endpoint detail_persons_work, & fix: added person validation in create_persons_work

--- app/routers/works.py
+++ app/routers/works.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from sqlmodel import select
 
 from db import SessionDep
-from models import Work, WorkCreate
+from models import Person, Work, WorkCreate
 
 router = APIRouter()
 
@@ -11,8 +11,12 @@ async def list_persons_work(person_id: int, session: SessionDep):
     works = session.exec(select(Work).where(person_id == person_id)).all()
     return works
 
-@router.post('/persons/{person_id}/works/', tags=['Work'])
+@router.post('/persons/{person_id}/works/', tags=['Work'], status_code=status.HTTP_201_CREATED)
 async def create_persons_work(person_id: int, work_data: WorkCreate, session: SessionDep):
+    person = session.get(Person, person_id)
+    if not person:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Person not found.")
+    
     work_dict = work_data.model_dump()
     work_dict['person_id'] = person_id
     work = Work.model_validate(work_dict)
@@ -23,3 +27,11 @@ async def create_persons_work(person_id: int, work_data: WorkCreate, session: Se
         "message": "Person's work inserted correctly.",
         "data": work
             }
+    
+
+@router.get('/persons/works/{work_id}', tags=['Work'])
+async def detail_persons_work(work_id: int, session: SessionDep):
+    work = session.get(Work, work_id)
+    if not work:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Work not found.")
+    return work
